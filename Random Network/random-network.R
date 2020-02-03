99d48d57af0ee0cf9ff61ae5743b1470cc576c9f
Ankit Dimri <ankitdimri1999@gmail.com>
2020-02-04 01:08:34 +0530

    random network created

--- Random Network/random-network.R
+++ Random Network/random-network.R
@@ -1,6 +1,6 @@
 library(igraph)
 
-Rfb = erdos.renyi.game(4039, 0.0108) #fb graph with number of nodes 4039
+Rfb = erdos.renyi.game(5000, 0.03)
 t = table(degree(Rfb))
 # Plotting the frequency plot of degree of each node
 plot(t / sum(t),xlab="Degree", ylab="Frequency")
