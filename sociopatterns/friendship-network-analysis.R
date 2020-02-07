adc43d8b8effbb6b3cf96084e2b0fc0810dfc32f
Ankit Dimri <ankitdimri1999@gmail.com>
2020-02-07 11:40:15 +0530

    gender added through metadata

--- sociopatterns/friendship-network-analysis.R
+++ sociopatterns/friendship-network-analysis.R
@@ -5,16 +5,34 @@ G = read_graph("~/ankit/Github/Social-Network-Analysis/sociopatterns/Friendship-
 # plotting the graph
 tkplot (G, vertex.size = 2, vertex.label = NA, edge.color = "blue", vertex.color = "grey", edge.width = 0.5, layout = layout_with_kk)
 
+# Read metadata
+meta = read.csv ("~/ankit/Github/Social-Network-Analysis/sociopatterns/metadata_2013.txt", header = F, sep = "\t")
+
+# Metadata which does not map to an existing vertex: delete them
+for (i in meta [,1]) {
+  if (!(i %in% V(G))) {
+    print (meta [which (meta$V1 == i),1])
+    r <- with (meta, which (V1 == i, arr.ind = T))
+    meta = meta [-r,]
+  }
+}
+
 # converting the graph into undirected graph of mutual friends
 UG = as.undirected (G, mode="mutual")
+
+# Assign genders
+V(UG)[meta [,1]]$gender = meta [,3]
+# Delete vertices which do not have gender specified
+UGG = delete.vertices (UG, V(UG)[is.na(V(UG)$gender)])
+
 # plot the undirected graph
-tkplot (UG, vertex.size = 2, vertex.label = NA, edge.color = "blue", vertex.color = "grey", edge.width = 0.5, layout = layout_with_kk)
+tkplot (UGG, vertex.size = 2, vertex.label = NA, edge.color = "blue", vertex.color = "grey", edge.width = 0.5, layout = layout_with_kk)
 
 # components in the undirected graph
-comps = components(UG)
+comps = components(UGG)
 
 # decompose the graph into components
-comp = decompose(UG)
+comp = decompose(UGG)
 
 # plot the biggest component
 g = comp [[2]]
