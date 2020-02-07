6ad5cfbd93e6ba7ae57cda7782adcd4569bb7bcc
Ankit Dimri <ankitdimri1999@gmail.com>
2020-02-07 21:23:09 +0530

    added degree destribution plot for UG

--- sociopatterns/friendship-network-analysis.R
+++ sociopatterns/friendship-network-analysis.R
@@ -20,11 +20,27 @@ for (i in meta [,1]) {
 # converting the graph into undirected graph of mutual friends
 UG = as.undirected (G, mode="mutual")
 
+# Print degrees of the graph
+V(UG)$degree = igraph::degree (UG)
+V(UG)$degree
+# Delete vertices with 0 degree
+Gd = delete.vertices (UG, V(UG) [V(UG)$degree == 0])
+# Plot this graph
+tkplot (Gd, vertex.size = 2, vertex.label = NA, edge.color = "blue", vertex.color = "grey", edge.width = 0.5, layout = layout_with_kk)
+# Plot degree distribution
+t = table (V(Gd)$degree)
+plot (t)
+
+# Finding and printing the closeness of the graph
+V
+
 # Assign genders
 V(UG)[meta [,1]]$gender = meta [,3]
 # Delete vertices which do not have gender specified
 UGG = delete.vertices (UG, V(UG)[is.na(V(UG)$gender)])
+UGG = delete.vertices (UGG, V(UGG)$gender == "3")
 
+V(UGG)$type = ifelse(V(UGG)$gender == "1", T , F)
 # plot the undirected graph
 tkplot (UGG, vertex.size = 2, vertex.label = NA, edge.color = "blue", vertex.color = "grey", edge.width = 0.5, layout = layout_with_kk)
 
