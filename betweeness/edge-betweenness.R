3fb09ad634ebf1f4e03d032d3e7a8f86cde99c6e
Ankit Dimri <ankitdimri1999@gmail.com>
2020-03-23 19:51:30 +0530

    plot communities

--- betweeness/edge-betweenness.R
+++ betweeness/edge-betweenness.R
@@ -4,22 +4,38 @@ G = read.csv("~/ankit/Github/Social-Network-Analysis/dolphin.csv")
 G = graph_from_data_frame (G, directed = FALSE)
 
 
-find_community <-function (G) {
-  com = components (G)$no
+find_community <- function (g) {
+  com = components (g)$no
   
   while (com == 1) {
     
     cat ("Number of components: ", com, "\n")
-    ebt = edge_betweenness (G)  
-    G = G -  E(G) [which.max (ebt)]
-    com = components (G)$no
+    ebt = edge_betweenness (g)  
+    g = g -  E(g) [which.max (ebt)]
+    com = components (g)$no
   }
   
   cat ("Number of components: ", com, "\n")
   
-  com1 = groups (components (G)) [1]
-  com2 = groups (components (G)) [2]
+  com1 = groups (components (g)) [1]
+  com2 = groups (components (g)) [2]
   comps = c(c(com1), c(com2))
   
   return (comps)
 }
+
+show_communities <- function (G) {
+  
+  comps = find_community (G)
+  
+  c1 = comps$`1`
+  c2 = comps$`2`
+  
+  V(G)$color = ifelse (V(G) %in% c1, "green", "blue")
+  
+  tkplot (G, vertex.color = V(G)$color)  
+}
+
+
+show_communities (G)
+  
