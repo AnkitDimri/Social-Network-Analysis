53172b4de7dd61cfdeebe40b5a63de5ee71b21e3
Ankit Dimri <ankitdimri1999@gmail.com>
2020-03-30 19:59:35 +0530

    foci nodes added

--- fatman/fatman-evolutionary-model.R
+++ fatman/fatman-evolutionary-model.R
@@ -2,22 +2,24 @@ library (igraph)
 
 G = make_empty_graph (n = 100) 
 
-G = assign_bmi(G)
-
-labels = V(G)$name
-
+G = assign_bmi (G)
+G = add_foci_nodes (G)
 
 
 
-
-
-plot (G, labels = labels, vertex.size = V(G)$name - 10)
+plot (G, vertex.size = V(G)$size)
 
 
 
 assign_bmi <- function(g) {
   V (g)$name = as.integer (runif (length (V(g)), 15, 40))
+  V (g)$size = V (g)$name - 10
   V (g)$type = "person"
+  V (g)$shape = "circle"
   return (g)
 }
 
+add_foci_nodes <- function (lg) {
+  lg = lg + vertices (name = c("gym", "eatout", "movie club", "karate club", "yoga club"), shape = "rectangle", size = 25, type = "foci")
+  return (lg)
+}
