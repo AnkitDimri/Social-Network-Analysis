243ceec7de625516b071f15a75117041975bdbe5
Ankit Dimri <ankitdimri1999@gmail.com>
2020-03-30 21:13:20 +0530

    homophili added

--- fatman/fatman-evolutionary-model.R
+++ fatman/fatman-evolutionary-model.R
@@ -6,7 +6,7 @@ G = assign_bmi (G)
 G = add_foci_nodes (G)
 G = add_foci_edges (G)
 G = get_color (G)
-
+G = add_homophili(G)
 
 
 plot (G, vertex.size = V(G)$size, vertex.shape = V (G)$shape, layout = layout_with_kk)
@@ -14,8 +14,9 @@ plot (G, vertex.size = V(G)$size, vertex.shape = V (G)$shape, layout = layout_wi
 
 
 assign_bmi <- function (lg) {
-  V (lg)$name = as.integer (runif (length (V(lg)), 15, 41))
-  V (lg)$size = V (lg)$name - 10
+  V (lg)$name = V (lg)
+  V (lg)$bmi = as.integer (runif (length (V(lg)), 15, 41))
+  V (lg)$size = V (lg)$bmi - 10
   V (lg)$type = "person"
   V (lg)$color = "yellow"
   V (lg)$shape = "circle"
@@ -23,7 +24,7 @@ assign_bmi <- function (lg) {
 }
 
 add_foci_nodes <- function (lg) {
-  lg = lg + vertices (name = c("gym", "eatout", "movie club", "karate club", "yoga club"), shape = "rectangle", size = 25, type = "foci", color = "blue")
+  lg = lg + vertices (name = c("gym", "eatout", "movie club", "karate club", "yoga club"), shape = "rectangle", size = 25, type = "foci", color = "blue", bmi = 0)
   return (lg)
 }
 
@@ -41,11 +42,11 @@ add_foci_edges <- function (lg) {
 get_color <- function (lg) {
   for (i in 1:length (V(lg))) {
     if (V (lg)$type [i] == "person") {
-      if (V (lg)$name [i] > 32) {
+      if (V (lg)$bmi [i] > 32) {
         V (lg)$color [i] = "red"
       }
       else {
-        if (V (lg)$name [i] > 22) {
+        if (V (lg)$bmi [i] > 22) {
           V (lg)$color [i] = "orange"
         }
         else {
@@ -54,6 +55,28 @@ get_color <- function (lg) {
       }
     }
   }
+  
+  return (lg)
+}
+
+add_homophili <- function (lg) {
+  
+  pnodes = V(lg) [V(lg)$type == "person"]
+  
+  for (i in 1:length (pnodes)) {
+    for (j in 1:length (pnodes)) {
+      if (i != j) {
+        diff = abs (pnodes$bmi [i] - pnodes$bmi [j])
+        prob = 1 / (diff + 1000)
+        
+        if (runif (1, 0, 1) < prob) {
+          lg = lg + edge (pnodes [i], pnodes [j])
+        }
+      }
+    }
+  }
+  
+  lg = as.undirected (lg, mode = "collapse")
   
   return (lg)
 }
