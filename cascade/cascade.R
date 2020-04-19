2597bc2eeb85ef5e054c56d1e18e24f200ca6b48
Ankit Dimri <ankitdimri1999@gmail.com>
2020-04-20 00:11:19 +0530

    loop

--- cascade/cascade.R
+++ cascade/cascade.R
@@ -1,6 +1,6 @@
 library (igraph) # header file
 
-cascade <- function (graph, frmt) {
+cascade <- function (graph, frmt, threshold) {
   
   G = read_graph (graph, format = frmt)
   V (G)$accepted = "B"
@@ -14,6 +14,16 @@ cascade <- function (graph, frmt) {
   G = seeder (G)
   G = color (G)
   plot (G, vertex.color = V (G)$color)
+  
+  # start cascading
+  G = spread (G, threshold)
+  
+  while (G$change == T) {
+    G = color (G)
+    plot (G, vertex.color = V (G)$color)
+    G = spread (G, threshold)
+  }
+  
 }
 
 seeder <- function (lg) {
@@ -33,6 +43,32 @@ color <- function (lg) {
   return (lg)
 }
 
+spread <- function (lg, t) {
+  
+  s = c ()
+  flag = 0
+  for (i in 1:length (V (lg))) {
+    n = neighbors (lg, V (lg) [i])
+    count = 0
+    
+    for (j in 1:length (n))
+      if (V (lg) [n [j]]$accepted == "A")
+        count = count + 1
+    if (count / length (n) >= t) {
+      s = c (s, i)
+      flag = 1
+    }
+  }
+  
+  V (lg) [s]$accepted = "A"
+  if (flag)
+    lg$change = T
+  else
+    lg$change = F
+  
+  return (lg)
+}
 
-cascade ("../karate.gml", "gml")
+
+cascade ("../karate.gml", "gml", 0.3)
 
